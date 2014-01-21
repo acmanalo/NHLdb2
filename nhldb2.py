5697c65dc24171dda422af70f64479a651385bc6
Andrew Manalo <acmanalo@gmail.com>
2014-01-20 22:16:18 -0800

    Redid get_stats to output ordering.

--- nhldb2.py
+++ nhldb2.py
@@ -5,52 +5,83 @@ from time import strptime
 import requests
 import re
 
-
 GAME_BASE_URL = "http://www.nhl.com/gamecenter/en/boxscore?id="
 
-def get_game_stats(game_id):
+def get_stats(game_id):
     '''
     Takes game id and parses corresponding box score html
 
     
-    returns [aTeam, aGoals, aPP goal, aPP opp, aHits, aFOW, aGiveaways,
-             aTakeaways, aBlocks, aPIM, hTeam, hGoals, hPP goals, hPP opp,
-             hHits, hFOW, hGiveaways, hTakeaways, hBlocks, hPIM]
+    Returns array in order specified by output_order
     '''
-    stat_out = []
-    
-    url = requests.get(GAME_BASE_URL + str(game_id))
-    soup = BeautifulSoup(url.content)
 
-    stats_to_find = ['score', 'at', 'aPP', 'aHits', 'aFOW', 'aGive',
-                     'aTake', 'aBlock', 'aPIM', 'ht', 'hPP','hHits',
-                     'hFOW', 'hGive', 'hTake', 'hBlock', 'hPIM']
+    def parse_fraction(fraction):
+        return str(fraction).split('/')
     
-    game = soup.find_all(True, {'class' : stats_to_find })
-    
-    stat_out.append(str(game[2].contents[0]))
-    stat_out.append(int(game[0].contents[0]))
+    try:
+        url = requests.get(GAME_BASE_URL + str(game_id))
+        soup = BeautifulSoup(url.content)
 
-    [pp_goal, pp_opp] = str(game[6].contents[0]).split('/')
-    stat_out.append(int(pp_goal))
-    stat_out.append(int(pp_opp))
-    
-    for i in range(8, 17, 2):
-        stat_out.append(int(game[i].contents[0]))
+        stats_to_find = ['score', 'at', 'aPP', 'aHits', 'aFOW', 'aGive',
+                         'aTake', 'aBlock', 'aPIM', 'aShots',
+                         'ht', 'hPP','hHits','hFOW', 'hGive', 'hTake', 'hBlock',
+                         'hShots', 'hPIM']
+        
+        game = soup.find_all(True, {'class' : stats_to_find })
+        stats = {}
+        
+        aPeriod = hPeriod = 1
+        
+        for stat in game:
+            if len(stat['class'])<2:
+                c = stat['class'][0]
+            else:
+                c = '_'.join(stat['class'])
 
-    stat_out.append(str(game[3].contents[0]))
-    stat_out.append(int(game[1].contents[0]))
+            if stat['class'][0] == 'aShots':
+                c = 'aShots' + `aPeriod`
+                aPeriod+=1
+            elif stat['class'][0] == 'hShots':
+                c = 'hShots' + `hPeriod`
+                hPeriod+=1
 
-    [pp_goal, pp_opp] = str(game[7].contents[0]).split('/')
-    stat_out.append(int(pp_goal))
-    stat_out.append(int(pp_opp))
-    
-    for i in range(9, 18, 2):
-        stat_out.append(int(game[i].contents[0]))
-    
-    return stat_out
+            stats[str(c)] = str(stat.get_text())
 
-def get_date(date_string):
+        [aPPg, oPPo] = parse_fraction(stats['aPP'])
+        [hPPg, hPPo] = parse_fraction(stats['hPP'])
+
+        stats['aPPg'] = aPPg
+        stats['aPPo'] = oPPo
+        stats['hPPg'] = hPPg
+        stats['hPPo'] = hPPo
+
+        del stats['aPP']
+        del stats['hPP']
+
+        # Convert values in dictionary to int if possible
+        for val in stats:
+            try:
+                stats[val] = int(stats[val])
+            except:
+                continue
+            
+        output_order = ['t', 'Shots1', 'Shots2', 'Shots3', 'PPg', 'PPo', 'Hits',
+                        'FOW', 'Give', 'Take', 'Block', 'PIM']
+        
+        stats_out = []
+        for site in ['a', 'h']:         #append away stats then home
+            for i in output_order:      #append stats in order of output_order
+                stats_out.append(stats[site + i])
+
+        stats_out.insert(len(stats_out)/2 + 1, stats['score_hm'])
+        stats_out.insert(1, stats['score_aw'])
+        
+            
+        return stats_out
+    except:
+        return "Failed. :("
+
+def parse_date(date_string):
     '''
     Parse the date from NHL.com's format of dates
 
@@ -58,8 +89,17 @@ def get_date(date_string):
     '''
     date = strptime(date_string, "%a %b %d, %Y")
     return int(`date.tm_year` + str(date.tm_mon).zfill(2) + `date.tm_mday`)
-        
-print get_date("Mon Jan 10, 2014")
+
+start = 2013020001
+
+for i in range(start, start + 100):
+    print get_stats(i)
+
+
+##def get_game(
+##        
+
+
 ##def get(soup, class_name):
 ##    return str(soup.find(class_=class_name).contents[0])
 ##
